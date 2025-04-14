3be4f57ff02c52d94fbaa019e1279f02b9455eaa
U1410762 <u1410762@utah.edu>
2025-04-14 13:49:56 -0600

    fixed typo

--- add_routes.sh
+++ add_routes.sh
@@ -16,7 +16,7 @@ docker exec -it cs4480_pa3-r2-1 route add -net 10.0.19.0/24 gw 10.0.18.4
 
 # R3
 docker exec -it cs4480_pa3-r3-1 route add -net 10.0.14.0/24 gw 10.0.17.3
-docker exec -it cs4480_pa3-r3-1 add -net 10.0.14.0/24 gw 10.0.18.3
+docker exec -it cs4480_pa3-r3-1 route add -net 10.0.14.0/24 gw 10.0.18.3
 
 # R4
 docker exec -it cs4480_pa3-r4-1 route add -net 10.0.14.0/24 gw 10.0.16.4
