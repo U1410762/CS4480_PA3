941904bd830e5814265dc40e464781446a88d86d
U1410762 <u1410762@utah.edu>
2025-04-19 17:30:09 -0600

    Update network_setup.sh

--- network_setup.sh
+++ network_setup.sh
@@ -3,3 +3,8 @@
 bash ./dockersetup
 sudo docker compose up -d
 bash ./add_routes.sh
+
+sudo docker exec -it cs4480_pa3-r1-1 service frr restart
+sudo docker exec -it cs4480_pa3-r2-1 service frr restart
+sudo docker exec -it cs4480_pa3-r3-1 service frr restart
+sudo docker exec -it cs4480_pa3-r4-1 service frr restart
