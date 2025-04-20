5ac11645eb755f36d288887807b6d69cf7d3978e
U1410762 <u1410762@utah.edu>
2025-04-20 10:57:06 -0600

    help

--- route_through_r2.sh
+++ route_through_r2.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-sudo docker exec -it cs4480_pa3-r2-1 vtysh && interface eth0 && ip ospf cost 5 && interface eth1 && ip ospf cost 5 && exit
+sudo docker exec -i cs4480_pa3-r2-1 vtysh << EOF
+configure terminal
+interface eth0
+ ip ospf cost 5
+interface eth1
+ ip ospf cost 5
+end
+EOF
 
-sudo docker exec -it cs4480_pa3-r4-1 vtysh && interface eth0 && ip ospf cost 10 && interface eth1 && ip ospf cost 10 && exit
+sudo docker exec -i cs4480_pa3-r4-1 vtysh << EOF
+configure terminal
+interface eth0
+ ip ospf cost 10
+interface eth1
+ ip ospf cost 10
+end
+EOF
