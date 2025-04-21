8c5bb8bd5b38e7fe3ec0aaaa0cc09967a820c52e
U1410762 <u1410762@utah.edu>
2025-04-21 07:47:34 -0600

    Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -5,19 +5,21 @@ import sys
 
 args = sys.argv
 
-if "help" in args:
+if "-h" in args:
     print("Use 'setup' argument to set up the network.\nUse 'north' to switch traffic to the north interface (route through r2).\nUse 'south' to switch traffic to the south interface (route through r4)")
 
-elif "setup" in args:
+elif "-setup" in args:
     print("Setting up network...")
     os.chmod("./network_setup.sh", stat.S_IRWXO)
     # set up network
     subprocess.call("./network_setup.sh")
 
-elif "north" in args:
+elif "-north" in args:
+    print("Routing north...")
     os.chmod("./route_through_r2.sh", stat.S_IRWXO)
     subprocess.call("./route_through_r2.sh")
 
-elif "south" in args:
+elif "-south" in args:
+    print("Routing south...")
     os.chmod("./route_through_r4.sh", stat.S_IRWXO)
     subprocess.call("./route_through_r4.sh")
