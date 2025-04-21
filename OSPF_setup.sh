2463d26b459698fe2be24c0825b8b6aa97bc9b1c
U1410762 <u1410762@utah.edu>
2025-04-21 07:51:22 -0600

    Update OSPF_setup.sh

--- OSPF_setup.sh
+++ OSPF_setup.sh
@@ -7,4 +7,3 @@ apt install lsb-release
 FRRVER="frr-stable"
 echo deb '[signed-by=/usr/share/keyrings/frrouting.gpg]' https://deb.frrouting.org/frr \ $(lsb_release -s -c) $FRRVER | tee -a /etc/apt/sources.list.d/frr.list
 apt update && apt -y install frr frr-pythontools
-apt -y install nano
